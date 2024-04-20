3931bd77201d5f2b757ac0bbe46526c27c4cdf1f
isaibau <isaisabi02@gmail.com>
2024-04-20 13:39:39 -0600

    Se arreglo CSS de la página de inicio y de reviews

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,19 @@
 @extends('plantillaGeneral')
-
 @section('contenido')
 
 <div class="container-fluid p-5 bg-secondary fondo">
     <h1 class="title"><b>Reseñas destacadas</b></h1>
     <p class="montserrat-regular fs-4">Descubre las últimas y más populares reseñas negativas de nuestra comunidad.</p>
-    <button class="boton"><a href="{{ auth()->check() ? '/review' : '/register' }}" class="links">Añadir reseña</a></button>
+    <button id="reviewButton" class="boton">Añadir reseña</button>
 </div>
-
+<script>
+  var isAuthenticated = {{ auth()->check() ? 'true' : 'false' }};
+</script>
 <div class="container-fluid">
-<div class="container pt-5">
+<div class="container-fluid p-5">
     <h3><b>Peores establecimientos de la semana</b></h3>
 <br>
-    <div class="container">
+    <div class="container-fluid p-0">
         <div class="row">
           <div class="col">
             <div class="card mb-3">
@@ -99,12 +100,12 @@
   <i class="fa-solid fa-fire-flame-curved d-inline fa-2xl"></i><br>
 <br>
 
-  <div class="container">
+  <div class="container-fluid bg-transparent p-0">
     <div class="row" style="min-height: 300px;">
       <div class="col">
         <div class="card mb-3">
           <a href="/vistaReview" class="links-view">
-            <div class="row g-0" style="min-height: 300px"> 
+            <div class="col-md-8" style="min-height: 300px"> 
               <div class="col-md-8">
                 <div class="card-body" >
                   <h5 class="card-title"><b>Little Caesar's</b></h5> <br>
@@ -138,7 +139,7 @@
       <div class="col">
         <div class="card mb-3">
           <a href="/vistaReview" class="links-view">
-            <div class="row g-0">
+            <div class="col-md-8">
               <div class="col-md-8">
                 <div class="card-body">
                   <h5 class="card-title"><b>Restaurante Caro</b></h5> <br>
--- resources/views/plantillaGeneral.blade.php
+++ resources/views/plantillaGeneral.blade.php
@@ -40,12 +40,11 @@
         map.setView({ bounds: result.bestView });
     }
     </script>
-
         <title>Don't buy here</title>
     </head>
     <body>
     <header id="header">
-        <nav class="navbar1">
+        <nav class="navbar1 container-fluid bg-transparent px-5 pt-4">
             <div id="branding">
                 <form action="" id="content_search">
                     <button type="submit" id="btnSearch" name="btnSearch"><span class="icon-search"></span></button>
@@ -53,22 +52,20 @@
                 </form>
             </div>
             <div class="nav-links">
-                <ul>
-                    <li><a href="/">Inicio</a></li>
-                    <li><a href="/restaurant">Restaurantes</a></li>
-                    <li><a href="/shop">Tiendas</a></li>
-                    <!-- <li><a href="#" class="iconUser"><i class="icon-user"></i></a><a href="#" class="textUser">Perfil</a></li>
-                    <li><a href="#" class="btnLogin"><button class="btnLogin">Login</button></a><a href="#" class="textLogin">Login</a></li> -->
+                <ul class="m-0">
+                    <li class="liNavBar"><a href="/">Inicio</a></li>
+                    <li class="liNavBar"><a href="/restaurant">Restaurantes</a></li>
+                    <li class="liNavBar"><a href="/shop">Tiendas</a></li>
                     <?php
-                        $usuario_iniciado = false; 
-                        if ($usuario_iniciado) {
-                            echo '<li><a href="login" class="iconUser"><i class="icon-user"></i></a><a href="#" class="textUser">Perfil</a></li>';
+                        if (Auth::check()) {
+                            echo '<li><a href="/profile" class="iconUser"><i class="icon-user"></i></a><a href="/profile" class="textUser">Perfil</a></li>';
                         } else {
-                            echo '<li><a href="login" class="btnLogin" ><button class="btnLogin">Login</button></a><a href="#" class="textLogin">Login</a></li>';
+                            echo '<li><a href="/login" class="btnLogin"><button class="btnLogin">Login</button></a><a href="/login" class="textLogin">Login</a></li>';
                         }
                     ?>
                 </ul>
             </div>
+            
             <div class="menu-trigger">
                 <div class="menu-icon">
                     <div class="barTop"></div>
@@ -91,7 +88,8 @@
             </section><br>
             <p class="legal">©2024 NoCompresAqui. Todos los derechos reservados</p>
         </footer>
-        {{-- <script src="js/bsbase.js"></script> --}}
+        <script src="js/bsbase.js"></script>
+        <script src="js/redirect.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="https://kit.fontawesome.com/f87e73225e.js" crossorigin="anonymous"></script>
 
